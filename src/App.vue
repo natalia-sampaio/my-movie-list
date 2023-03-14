b5ac3d4beff837101acb084e56618184a2a0e491
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2023-03-13 23:36:03 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
