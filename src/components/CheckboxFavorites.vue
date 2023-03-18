55b0cfca0de869b2b95cfabd0d4ec92b20550544
Natália Sampaio Rosa e Silva <natalia.srs@outlook.com>
2023-03-18 17:11:42 -0300

    :recycle: refactor: better use of components

---
+++ src/components/CheckboxFavorites.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+    checked: Boolean
+})
+</script>
+
+
+<template>
+    <label for="filter" class="text-beige font-medium pl-2 peer-checked:text-blue-accent peer-checked:font-semibold hover:cursor-pointer">
+        <input :checked="checked" type="checkbox" class="h-4 w-4 hover:cursor-pointer peer accent-blue-accent checked: " id="filter" aria-label="show only my favorite movies"> Show only my favorite movies
+    </label>
+</template>
